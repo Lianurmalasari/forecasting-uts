e54eb645ffb0c8035ef0ede2701826050b8037fb
Lianurmalasari <145843965+Lianurmalasari@users.noreply.github.com>
2023-10-31 23:24:47 +0700

    Create forecast-ses.py

---
+++ forecast-ses.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+from statsmodels.tsa.holtwinters import SimpleExpSmoothing
+
+# Muat model Simple Exp Smoothing dari file .sav
+model_file = 'forecast-ses.sav'
+model = pickle.load(open(model_file, 'rb'))
+
+# Muat dataset avocado.csv
+data_file = 'avocado.csv'
+data = pd.read_csv(data_file)
+data['Date'] = pd.to_datetime(data['Date'])
+data.set_index('Date', inplace=True)
+
+# Judul aplikasi
+st.title('Forecasting Rata-Rata Harga Alpukat')
+
+# Slider untuk menentukan jumlah bulan yang akan diprediksi
+forecast_steps = st.slider('Jumlah Minggu Prediksi', 1, 30, 12)
+
+# Tombol "Prediksi"
+if st.button('Prediksi'):
+    # Prediksi dengan model Simple Exp Smoothing
+    forecast = model.forecast(steps=forecast_steps)
+    
+    # Tampilkan data asli
+    st.subheader('Data Asli')
+    st.line_chart(data)
+
+    # Tampilkan hasil prediksi
+    st.subheader('Hasil Prediksi')
+    st.line_chart(forecast)
+        
+    # Tampilkan grafik data asli dengan hasil prediksi
+    st.subheader('Grafik Data Asli dengan Hasil Prediksi')
+    fig, ax = plt.subplots()
+    data['AveragePrice'].plot(style='--', color='gray', legend=True, label='Data Asli', ax=ax)
+    forecast.plot(color='b', legend=True, label='Prediksi', ax=ax)
+    st.pyplot(fig)
+    
+    # Tampilkan tabel hasil prediksi
+    st.subheader('Tabel Hasil Prediksi')
+    forecast_df = pd.DataFrame({
+    'Tanggal': pd.date_range(start=data.index[-1], periods=forecast_steps, freq='W'),
+    'Prediksi': forecast
+    })
+    st.dataframe(forecast_df)
